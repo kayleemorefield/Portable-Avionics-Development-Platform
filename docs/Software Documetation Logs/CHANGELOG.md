2ea7c5687abf67e3b4d9d66a9d842c6a609e3fe7
Kaylee Morefield <YOUR_GITHUB_EMAIL@example.com>
2026-07-18 22:30:18 -0400

    Updated CHANGELOG and voltage-to-pressure logic

--- docs/Software Documetation Logs/CHANGELOG.md
+++ docs/Software Documetation Logs/CHANGELOG.md
@@ -6,9 +6,15 @@
 - Established project folder structure
 - Added airspeed module
 - Connected local repository to GitHub
-- Updated word doc up to 2.3
+- Updated project report up to 2.3
 ---
 
 ## v0.1.1 - 2026-07-14
 
- 
+ - Soldered headers to ADS1115 
+ - Assembled airspeed system prototype
+ - Created wiring diagram for airspeed prototype
+ - Implemented Python code to read pressure sensor voltage
+ - Added voltage-to-pressure Python logic
+ - Declared variables in airspeed.py for Bernoulli's Equation
+ - Updated project report with Section 1.1
--- systems/airspeed.py
+++ systems/airspeed.py
@@ -1,12 +1,27 @@
 import math, board, busio, time
-import adafruit_ads1x15.ads1115 as ADS
+import adafruit_ads1x15.ads1115 as ads
 
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c)
+ads = ads.ADS1115(i2c)
 from adafruit_ads1x15.analog_in import AnalogIn
 pressure_sensor = AnalogIn(ads, 0)
-voltage = pressure_sensor.voltage
 
-while True:
-    print(f"Voltage: {pressure_sensor.voltage:.3f} V")
+while True:     # loop to constantly check voltage input
+    voltage = pressure_sensor.voltage
+
+    print(f"Voltage: {voltage:.3f} V")
     time.sleep(0.5)
+
+#    Voltage to Pressure
+
+def voltage_to_pressure(voltage):
+    pressure = (voltage - 2.713) * SENSOR_GAIN
+    return pressure
+
+#    Bernoulli's Equation
+
+rho = 1.225
+delta_p = voltage_to_pressure(voltage) # h in Bernoulli's Eq
+airspd_Pa = math.sqrt((2 * h) / rho)
+airspd_kPa = airspd_Pa * 1000
+
