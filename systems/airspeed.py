adbab53f66059256218d50fead9a59a2f1e9493c
Kaylee Morefield <YOUR_GITHUB_EMAIL@example.com>
2026-07-18 20:37:57 -0400

    deleted initialization in airspeed.py

--- systems/airspeed.py
+++ systems/airspeed.py
@@ -1,8 +1,6 @@
 import math, board, busio, time
 import adafruit_ads1x15.ads1115 as ADS
 
-pressure_sensor = 0 # initialize A0 on ads1115
-
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 from adafruit_ads1x15.analog_in import AnalogIn
