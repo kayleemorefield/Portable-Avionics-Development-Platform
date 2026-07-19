f60b0714238820e16922760a3e59387c0f1526f6
Kaylee Morefield <YOUR_GITHUB_EMAIL@example.com>
2026-07-18 20:36:34 -0400

    Added loop to airspeed.py

--- systems/airspeed.py
+++ systems/airspeed.py
@@ -6,5 +6,9 @@ pressure_sensor = 0 # initialize A0 on ads1115
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 from adafruit_ads1x15.analog_in import AnalogIn
-pressure_sensor = AnalogIn(ads, ADS.P0)
+pressure_sensor = AnalogIn(ads, 0)
 voltage = pressure_sensor.voltage
+
+while True:
+    print(f"Voltage: {pressure_sensor.voltage:.3f} V")
+    time.sleep(0.5)
